6b3491a7dd6c90fc1e1cf31ca95d04189d03debc
Alexis Tyler <xo@wvvw.me>
2019-10-23 11:59:06 +1030

    improvement: add graphql client to main app

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue';
-import app from './src/app.vue';
+import { createClient, withProvider } from 'vue-gql';
+import App from './src/app.vue';
+
+const client = createClient({
+  url: 'http://localhost:5000/graphql'
+});
+
+// use this instead of your App
+const AppWithClient = withProvider(App, client);
 
 new Vue({
-  render: h => h(app)
+  render: h => h(AppWithClient)
 }).$mount('#app');
