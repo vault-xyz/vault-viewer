be9f8731ce8e89e0dfd0c567800f4b594b74887e
Alexis Tyler <xo@wvvw.me>
2019-10-23 11:56:27 +1030

    feat(components): add search-box

---
+++ src/components/search-box.vue
@@ -0,0 +1,29 @@
+<template>
+  <input v-model="localInput" v-bind="{ placeholder }" />
+</template>
+
+<script>
+export default {
+    name: 'SearchBox',
+    model: {
+        prop: 'input',
+        event: 'change'
+    },
+    data() {
+        return {
+            localInput: this.$props.input
+        };
+    },
+    props: {
+        placeholder: String
+    },
+    watch: {
+        localInput(value) {
+            this.$emit('change', value);
+        }
+    }
+};
+</script>
+
+<style>
+</style>
